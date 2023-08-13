d8b57a013c2d2364ac40fc87d359fbf40bc8cb64
Aliaksandr <sigai.aleksandr@mail.ru>
2023-08-13 12:00:56 +0300

    Получение информации о компаниях по API

---
+++ classes/abstract_class.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+
+import requests
+
+
+class Get_Info:
+    """
+    Класс для получения информации через API
+    """
+
+    @classmethod
+    def get_company_info(cls, company_id):
+        """
+        Метод для получения информации о компании
+        """
+
+        url = f'https://api.hh.ru/employers/{company_id}'
+        response = requests.get(url)
+        vacancies = response.json()
+        info_to_column = [{
+            'company_id': vacancies['id'],
+            'company_name': vacancies['name'],
+            'company_url': vacancies['site_url'],
+            'company_area': vacancies['area']['name'],
+            'company_industries': vacancies['industries'][0]['name'],
+            'quantity_open_vacancies': vacancies['open_vacancies']
+
+        }]
+        return info_to_column
+
+    @classmethod
+    def get_vacancies_info(cls, company_id):
+        """
+        Метод для получения информации об открытых вакансиях компании по её id
+        """
+
+        url = f'https://api.hh.ru/vacancies?employer_id={company_id}'
+        params = {
+            "per_page": 100
+        }
+        response = requests.get(url, params=params)
+        vacancies = response.json()
+        info = vacancies['items']
+        info_to_db = []
+        for x in info:
+            salary = x['salary']
+            salary_range = f"{salary['from']}-{salary['to']}, валюта {salary['currency']}" if salary and salary[
+                'from'] and salary['to'] else "Не указано"
+            date = x['published_at']
+            date_str = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z')
+            publish_time = date_str.strftime('%Y-%m-%d')
+            info_to_column = {'vacancy_id': x['id'],
+                              'vacancy_name': x['name'],
+                              'vacancy_url': x['url'],
+                              'vacancy_area': x['area']['name'],
+                              'vacancy_requirement': x['snippet']['requirement'],
+                              'vacancy_responsibility': x['snippet']['responsibility'],
+                              'vacancy_salary': salary_range,
+                              'vacancy_published_date': publish_time
+                              }
+            info_to_db.append(info_to_column)
+        return info_to_db
---
+++ classes/get_company_info.py
@@ -0,0 +1,116 @@
+from classes.abstract_class import Get_Info
+
+
+class Get_Companies_Info(Get_Info):
+    """
+    Реализация класса для полчения информации о компании
+    """
+    id_list = {
+        'yandex': 1740,
+        'sber': 3529,
+        'vk': 15478,
+        'alfa': 80,
+        'tinkoff': 78638,
+        'gaz_prom': 39305,
+        'vtb': 4181,
+        'sibur': 3809,
+        'tele_2': 4219,
+        'mts': 3776
+    }
+
+    def get_info_yd(self):
+        """
+        Получение информации яндекс
+        """
+        company_id = self.id_list['yandex']
+        info = self.get_company_info(company_id)
+        return info
+
+    def get_info_sb(self):
+        """
+        Получение информации сбер
+        """
+        company_id = self.id_list['sber']
+        yd = Get_Info()
+        info = yd.get_company_info(company_id)
+        return info
+    def get_info_vk(self):
+        """
+        Получение информации вк
+        """
+        company_id = self.id_list['vk']
+        yd = Get_Info()
+        info = yd.get_company_info(company_id)
+        return info
+    def get_info_al(self):
+        """
+        Получение информации альфа
+        """
+        company_id = self.id_list['alfa']
+        yd = Get_Info()
+        info = yd.get_company_info(company_id)
+        return info
+    def get_info_tk(self):
+        """
+        Получение информации тинькофф
+        """
+        company_id = self.id_list['tinkoff']
+        yd = Get_Info()
+        info = yd.get_company_info(company_id)
+        return info
+    def get_info_gz(self):
+        """
+        Получение информации газпром
+        """
+        company_id = self.id_list['gaz_prom']
+        yd = Get_Info()
+        info = yd.get_company_info(company_id)
+        return info
+    def get_info_vt(self):
+        """
+        Получение информации втб
+        """
+        company_id = self.id_list['vtb']
+        yd = Get_Info()
+        info = yd.get_company_info(company_id)
+        return info
+    def get_info_sr(self):
+        """
+        Получение информации сибур
+        """
+        company_id = self.id_list['sibur']
+        yd = Get_Info()
+        info = yd.get_company_info(company_id)
+        return info
+    def get_info_tl(self):
+        """
+        Получение информации теле 2
+        """
+        company_id = self.id_list['tele_2']
+        yd = Get_Info()
+        info = yd.get_company_info(company_id)
+        return info
+    def get_info_mt(self):
+        """
+        Получение информации мтс
+        """
+        company_id = self.id_list['mts']
+        yd = Get_Info()
+        info = yd.get_company_info(company_id)
+        return info
+
+    # def amount_info(self):
+    #     yd = self.get_info_yd()
+    #     sb = self.get_info_sb()
+    #     vk = self.get_info_vk()
+    #     al = self.get_info_al()
+    #     tk = self.get_info_tk()
+    #     gz = self.get_info_gz()
+    #     vt = self.get_info_vt()
+    #     sr = self.get_info_sr()
+    #     tl = self.get_info_tl()
+    #     mt = self.get_info_mt()
+    #     amount = yd, sb, vk, al, tk, gz, vt, sr, tl, mt
+    #     for x in amount:
+    #         print(x)
+
