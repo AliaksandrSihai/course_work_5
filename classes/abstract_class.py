77dc2df9d2b52b4127717f886c43b5f9624e08d5
Aliaksandr <sigai.aleksandr@mail.ru>
2023-08-15 22:16:11 +0300

    Изменение выборки количества вакансий и подключения к бд через database файл

--- classes/abstract_class.py
+++ classes/abstract_class.py
@@ -48,29 +48,34 @@ class GetInfo:
         """
 
         url = f'https://api.hh.ru/vacancies?employer_id={company_id}'
-        params = {
-            "per_page": 100
-        }
-        response = requests.get(url, params=params)
-        vacancies = response.json()
-        info = vacancies['items']
         info_to_db = []
-        for x in info:
-            salary = x['salary']
-            salary_range = salary['from'] if salary and salary[
-                'from'] else 0
-            date = x['published_at']
-            date_str = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z')
-            publish_time = date_str.strftime('%Y-%m-%d')
-            info_to_column = {'vacancy_id': x['id'],
-                              'vacancy_name': x['name'],
-                              'vacancy_url': x['url'],
-                              'vacancy_area': x['area']['name'],
-                              'vacancy_requirement': x['snippet']['requirement'],
-                              'vacancy_responsibility': x['snippet']['responsibility'],
-                              'vacancy_salary': salary_range,
-                              'vacancy_published_date': publish_time,
-                              'company_id': x['employer']['id']
-                              }
-            info_to_db.append(info_to_column)
+        page = 1
+        while page < 19:
+            page += 1
+            params = {
+                "per_page": 100,
+                "page": page
+            }
+
+            response = requests.get(url, params=params)
+            vacancies = response.json()
+            info = vacancies['items']
+            for x in info:
+                salary = x['salary']
+                salary_range = salary['from'] if salary and salary[
+                    'from'] else 0
+                date = x['published_at']
+                date_str = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z')
+                publish_time = date_str.strftime('%Y-%m-%d')
+                info_to_column = {'vacancy_id': x['id'],
+                                  'vacancy_name': x['name'],
+                                  'vacancy_url': x['url'],
+                                  'vacancy_area': x['area']['name'],
+                                  'vacancy_requirement': x['snippet']['requirement'],
+                                  'vacancy_responsibility': x['snippet']['responsibility'],
+                                  'vacancy_salary': salary_range,
+                                  'vacancy_published_date': publish_time,
+                                  'company_id': x['employer']['id']
+                                  }
+                info_to_db.append(info_to_column)
         return info_to_db
