97debf024fa4f40cbfab0f396e876fe8425812c1
Aliaksandr <sigai.aleksandr@mail.ru>
2023-08-15 22:14:57 +0300

    Добавление файлов: utils(переменная с путями к файлу), database в gitignore, config(парсер database файла)

---
+++ utils.py
@@ -0,0 +1,13 @@
+path_to_sql_script = 'functions/sql_script'
+id_list = {
+        'yandex': 1740,
+        'sber': 3529,
+        'vk': 15478,
+        'alfa': 80,
+        'tinkoff': 78638,
+        'gaz_prom': 39305,
+        'vtb': 4181,
+        'sibur': 3809,
+        'tele_2': 4219,
+        'mts': 3776
+    }
